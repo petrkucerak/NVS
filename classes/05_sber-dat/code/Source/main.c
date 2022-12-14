35c8f4c3e6e0c579c92150d2c979149717a2e508
Petr Kucera <petr@khome.cz>
2022-12-14 12:55:41 +0100

    Create Echo template with UART

--- classes/05_sber-dat/code/Source/main.c
+++ classes/05_sber-dat/code/Source/main.c
@@ -12,11 +12,15 @@
 /*Definitions*/
 
 /*Global variables*/
+uint16_t tmp;
 
 /*Function definitions*/
-void RCC_Configuration(void);
-void GPIO_Configuration(void);
-void Delay(vu32 nCount);
+static void RCC_Configuration(void);
+static void GPIO_Configuration(void);
+static void Delay(vu32 nCount);
+static void USART2_Configuration(void);
+static void USART_SendData(USART_TypeDef *USARTx, uint16_t Data);
+uint16_t SART_WaitToReceivedData(USART_TypeDef *USARTx);
 
 void SystemInit(void) {
   // Prazdna inicializacni metoda
@@ -26,17 +30,29 @@ void SystemInit(void) {
 int main(void) {
   RCC_Configuration();  // inicializace hodin
   GPIO_Configuration(); // inicializace GPIO
+  USART2_Configuration();
+
+  /* Clear TC flag */
+  USART2->SR &= 0xFFBF;
+
   /*Nekonecna smycka*/
   while (1) {
     if (GPIOA->IDR & 0x1) { // je PA0 stisknuto??
       GPIOC->BSRR |= 0x100; // ANO rozsvitime LED na PC8
+
+      USART_SendData(USART2, 0x61);
+
     } else {
       GPIOC->BSRR |= 0x1000000; // ne zhasneme LED na PC8
     }
+
+    /* Echo template */
+    tmp = SART_WaitToReceivedData(USART2); // wait for a data
+    USART_SendData(USART2, tmp); // send data back
   }
 }
 /*Inicializace RCC*/
-void RCC_Configuration(void) {
+static void RCC_Configuration(void) {
   RCC->CR |= 0x10000; // HSE on
   while (!(RCC->CR & 0x20000)) {
   }
@@ -50,6 +66,7 @@ void RCC_Configuration(void) {
   RCC->CFGR &= 0xFFC3FFFF; // maskovani PLLMUL
   RCC->CFGR |= 0x1 << 18;  // Nastveni PLLMUL 3x
   RCC->CFGR |= 0x0 << 17;  // nastaveni PREDIV1 1x
+
   RCC->CFGR |= 0x10000;    // PLL bude clocovan z PREDIV1
   RCC->CFGR &= 0xFFFFFF0F; // HPRE=1x
   RCC->CFGR &= 0xFFFFF8FF; // PPRE2=1x
@@ -65,20 +82,73 @@ void RCC_Configuration(void) {
   while (!(RCC->CFGR & 0x00000008)) // je SYSCLK nastaveno?
   {
   }
+  /* Peripheral clock enable */
+  RCC->APB2ENR |= RCC_APB2ENR_IOPAEN; // enable PA
+  RCC->APB2ENR |= RCC_APB2ENR_IOPCEN; // enable PC
+  // RCC->APB2ENR |= 0x14;
 
-  RCC->APB2ENR |= 0x14; // pocoleni PA a PC
+  RCC->APB1ENR |= RCC_APB1ENR_USART2EN; // enable USART2 clock
 }
 /*Inicializace GPIO*/
-void GPIO_Configuration(void) {
+static void GPIO_Configuration(void) {
+  /* Blue light */
   GPIOC->CRH &= 0xFFFFFFF0; // PC8
   GPIOC->CRH |= 0x3;        // PC8 jako PP output
 
+  /* Blue button */
   GPIOA->CRL &= 0xFFFFFFF0;
   GPIOA->CRL |= 0x4; // PA0 jako Floating input
+
+  /* Configure PA2 (USART2 TX): alternate func. output Push-pull, 10MHz */
+  GPIOA->CRL &= 0xFFFFFF0F;
+  GPIOA->CRL |= 0x000000C0;
+
+  /* Configure PA3 (USART2 RX): floating input */
+  GPIOA->CRL &= 0xFFFFF0FF;
+  GPIOA->CRL |= 0x00000B00;
 }
 
-/*Delay smycka zpozduje zhruba o nCount tiku jadra*/
-void Delay(vu32 nCount) {
+/* Wait loop delays program flow for about nCount ticks */
+static void Delay(vu32 nCount) {
   for (; nCount != 0; nCount--)
     ;
+}
+static void USART2_Configuration(void) {
+  /* Baud Rate setup 9600 Bd (see Reference manual for details) */
+  USART2->BRR = 0x09C4;
+  /* 8 data bits */
+  USART2->CR1 &= 0xEFFF;
+  /* 1 stopbit */
+  USART2->CR2 &= 0xCFFF;
+
+  /* Parity disabled */
+  USART2->CR1 &= 0xFBFF;
+
+  /* Hardware flow control disable */
+  USART2->CR3 &= 0xFCFF;
+
+  /* Rx, TX enable */
+  USART2->CR1 |= 0x000C;
+
+  /* USART2 enable */
+  USART2->CR1 |= USART_CR1_UE;
+}
+
+static void USART_SendData(USART_TypeDef *USARTx, uint16_t Data) {
+  /* Is Transmit Data Register Empty */
+  while ((USARTx->SR & USART_SR_TXE) != USART_SR_TXE) {
+  }
+  /* Transmit a data */
+  // USARTx->DR = (Data & (uint16_t)0x01FF);
+  USARTx->DR = (Data);
+  /* Is Transmission Complete */
+  while ((USARTx->SR & USART_SR_TC) != USART_SR_TC) {
+  }
+}
+
+uint16_t SART_WaitToReceivedData(USART_TypeDef *USARTx) {
+  /* Is Read Data Register Not Empty */
+  while ((USARTx->SR & USART_SR_RXNE) != USART_SR_RXNE) {
+  }
+  return USARTx->DR;
 }
